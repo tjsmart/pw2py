b42f8a07fe42ed60644610a3e85701ccf0c72982
Tyler Smart <tjsmart@ucsc.edu>
2020-03-30 16:44:34 -0700

    Renamed Exampels

--- Example/QEInp/Ex1/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-delete="New_Si.sample.in  New_Si.sample.out input_tmp.in temp CRASH"
-for dlt in delete; do
-    rm -rf $dlt 2> /dev/null
-done
-echo "Clean"
--- Example/QEInp/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rm *.in.out 2> /dev/null
