c25d9be4ac92c1d76a726ca2749e21c9d3c49ae8
Tyler Smart <tjsmart@ucsc.edu>
2020-06-18 10:57:33 -0700

    Created setup.py, now can use pip to manage package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pw2py',
+    version='0.0.0',
+    description='python interface for reading quantum espresso input/output',
+    license=None,
+    long_description=long_description,
+    author='Tyler J. Smart',
+    author_email='tjsmart@ucsc.edu',
+    url="https://gitlab.com/tjsmart/pw2py",
+    packages=find_packages(),
+    install_requires=[
+        'f90nml',
+        'lxml',
+        'mendeleev',
+        'numpy',
+        'pandas',
+        'scipy'
+    ],
+    scripts=[
+        'scripts/conv_geo.py',
+        'scripts/cp_geo.py',
+        'scripts/linear_mix.py',
+    ]
+)
