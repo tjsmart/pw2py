c4fc599390019c5174dfe3524ac7dc7376d05f61
Tyler Smart <tjsmart@ucsc.edu>
2020-07-11 18:41:57 -0700

    Finished install script, now need to test things out

--- linter/install.sh
+++ linter/install.sh
@@ -7,27 +7,40 @@ help_and_quit(){
 }
 
 arg_parse(){
-    if [ -z "$1" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    if [ -z "$1" ]; then
         help_and_quit
     fi
-    # TODO parse other options (i.e. linking [l])
+    uselink=false
+    while [ -n "$1" ]; do
+        if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+            help_and_quit
+        elif [ "$1" == "-l" ] || [ "$1" == "--link" ]; then
+            uselink=true
+            shift
+        else
+            path_to_ale="$1"
+            break
+        fi
+    done
 }
 
 check_ale(){
     # check provided folder exists
-    if [ ! -d "$1" ]; then
-        echo "Folder provided does not exist: $1" >&2
+    if [ ! -d "$path_to_ale" ]; then
+        echo "Folder provided does not exist: $path_to_ale" >&2
         exit 1
     fi
     # check for registry file
-    registry_file="$1/autoload/ale/fix/registry.vim"
+    registry_file="$path_to_ale/autoload/ale/fix/registry.vim"
     if [ ! -f "$registry_file" ]; then
-        echo "Registry file not found: '$registry_file'"
+        echo "Registry file not found: '$registry_file'" >&2
+        exit 1
     fi
     # check for fixers folder
-    fixers_folder="$1/autoload/ale/fixers"
+    fixers_folder="$path_to_ale/autoload/ale/fixers"
     if [ ! -d "$fixers_folder" ]; then
-        echo "Fixers folder not found: '$fixers_folder'"
+        echo "Fixers folder not found: '$fixers_folder'" >&2
+        exit 1
     fi
 }
 
@@ -43,12 +56,12 @@ check_perl(){
 insert_after_pattern(){
     # check that ptrn is in file
     if ! grep -q "$ptrn" "$1"; then
-        echo "Desired pattern was not found in '$1', are you sure this is the right file?"
+        echo "Desired pattern was not found in '$1', are you sure this is the right file?" >&2
         exit 1
     fi
     # check if inst is already in file
     if grep -q "qelint" "$1"; then
-        echo "qelint already insterted into file '$1' ... skipping"
+        echo "qelint already insterted into file '$1' ... skipping" >&2
     else
         # otherwise insert 'inst' after 'ptrn'
         perl -i -pe "s/$ptrn/$ptrn$inst/;" $1
@@ -56,9 +69,34 @@ insert_after_pattern(){
 }
 
 cp_or_ln_qelint(){
-    # check for file
-    echo "TODO"
-    # TODO cp qelint.vim $fixers_folder
+    # check for file to copy
+    local vimfile="qelint.vim"
+    if [ ! -f $vimfile ]; then
+        echo "File 'qelint.vim' not found, exiting" >&2
+        exit 1
+    fi
+    # check for file destination
+    local file_dest="$fixers_folder/$vimfile"
+    if $uselink ; then
+        ln -fs $PWD/$vimfile $file_dest
+    else
+        cp $vimfile $file_dest
+    fi
+}
+
+configure_ale(){
+    check_ale
+    check_perl
+    insert_after_pattern $registry_file
+    cp_or_ln_qelint $fixers_folder
+}
+
+install_qelint(){
+    if $uselink ; then
+        ln -fs $PWD/qelint /usr/local/bin
+    else
+        cp qelint /usr/local/bin
+    fi
 }
 
 
@@ -70,11 +108,15 @@ inst="\n\
 \\\       'description': 'Format quantum espresso input files',\n\
 \\\   },"
 
+echo "Parsing user input"
 arg_parse $@
-check_ale $@
-check_perl
-insert_after_pattern $registry_file
-cp_or_ln_qelint $fixers_folder
+echo "installing qelint to /usr/local/bin"
+install_qelint
+echo "configuring users ale"
+configure_ale
+echo "script ran successfully!"
+echo "--------------------------"
+
 echo "Almost done! Don't forget to add these lines to your ~/.vimrc!"
 echo "let g:ale_fixers = {'espresso': ['qelint']}"
 echo "let g:ale_fix_on_save = 1"
