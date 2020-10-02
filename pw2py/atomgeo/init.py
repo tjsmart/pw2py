5f94ad29a124e04659cbc104c6d05503f42fab93
Tyler Smart <tjsmart@ucsc.edu>
2020-10-02 09:42:00 -0700

    Change ion type to object to correctly preserve str characteristics

--- pw2py/atomgeo/init.py
+++ pw2py/atomgeo/init.py
@@ -3,7 +3,7 @@ from numpy import array
 
 def __init__(self, ion=None, par=None, pos=None, par_units=None, pos_units=None):
     ''' initialize atomgeo instance '''
-    self._ion = array(ion, dtype=str)
+    self._ion = array(ion, dtype=object)
     self._par = array(par, dtype=float)
     self._pos = array(pos, dtype=float)
     self._par_units = str(par_units).lower()
