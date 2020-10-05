aeecc42500fdd84e74a4a80d01de94baf19b41af
Tyler Smart <tjsmart@ucsc.edu>
2020-10-05 09:52:49 -0700

    Bug fix for read_geo, reading atom_geo from a qe prefix was broken

--- pw2py/_common/resource.py
+++ pw2py/_common/resource.py
@@ -36,7 +36,8 @@ def _read_atomic_positions(lines, nat, no_if_pos=True):
         pos.append(np.array(nl[1:4], dtype=np.float64))
         if not no_if_pos:
             try:
-                if_pos.append(np.array([nl[4], nl[5], nl[6]], dtype=np.int))  # explicit so indexError will be thrown
+                # explicit so indexError will be thrown
+                if_pos.append(np.array([nl[4], nl[5], nl[6]], dtype=np.int))
             except IndexError:
                 if_pos.append(np.array([1, 1, 1], dtype=np.int))
     if no_if_pos:
@@ -59,13 +60,16 @@ def _ibrav_to_par(system_nml, units="angstrom"):
         par = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float64)
     elif int(system_nml['ibrav']) == 2:
         # v1 = (a/2)(-1,0,1),  v2 = (a/2)(0,1,1), v3 = (a/2)(-1,1,0)
-        par = np.array([[-1, 0, 1], [0, 1, 1], [-1, 1, 0]], dtype=np.float64) * 0.5
+        par = np.array([[-1, 0, 1], [0, 1, 1], [-1, 1, 0]],
+                       dtype=np.float64) * 0.5
     elif int(system_nml['ibrav']) == 3:
         # v1 = (a/2)(1,1,1),  v2 = (a/2)(-1,1,1),  v3 = (a/2)(-1,-1,1)
-        par = np.array([[1, 1, 1], [-1, 1, 1], [-1, -1, 1]], dtype=np.float64) * 0.5
+        par = np.array([[1, 1, 1], [-1, 1, 1], [-1, -1, 1]],
+                       dtype=np.float64) * 0.5
     elif int(system_nml['ibrav']) == 4:
         # v1 = a(1,0,0),  v2 = a(-1/2,sqrt(3)/2,0),  v3 = a(0,0,c/a)
-        par = np.array([[1, 0, 0], [-1/2, np.sqrt(3)/2, 0], [0, 0, 1]], dtype=np.float64)
+        par = np.array([[1, 0, 0], [-1/2, np.sqrt(3)/2, 0],
+                        [0, 0, 1]], dtype=np.float64)
     elif int(system_nml['ibrav']) == 6:
         # v1 = a(1,0,0),  v2 = a(0,1,0),  v3 = a(0,0,c/a)
         par = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float64)
@@ -73,7 +77,8 @@ def _ibrav_to_par(system_nml, units="angstrom"):
         # v1 = (a,0,0),  v2 = (0,b,0), v3 = (0,0,c)
         par = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float64)
     else:
-        raise ValueError("Unsupported value for ibrav '{}'".format(system_nml['ibrav']))
+        raise ValueError(
+            "Unsupported value for ibrav '{}'".format(system_nml['ibrav']))
 
     # rescale lattice parameters
     if 'a' in system_nml.keys():
@@ -116,7 +121,8 @@ def _convert_par(par, in_units, out_units="angstrom", alat=None, alat_units="ang
     elif out_units not in conversion:
         raise ValueError("Invalid value of out_units: '{}'".format(out_units))
     elif (in_units == "alat" or out_units == "alat") and alat is None:
-        raise ValueError("Requested alat conversion but value for alat was not provided.")
+        raise ValueError(
+            "Requested alat conversion but value for alat was not provided.")
     # calculate prefactor
     return par / np.float64(conversion[out_units.lower()]) * np.float64(conversion[in_units.lower()])
 
@@ -131,7 +137,8 @@ def _convert_pos(pos, in_units, out_units="angstrom", alat=None, alat_units="ang
     elif in_units != 'crystal' and out_units != 'crystal':
         return _convert_par(pos, in_units=in_units, out_units=out_units, alat=alat, alat_units=alat_units)
     elif par is None or par_units is None:
-        raise ValueError("Requested crystal conversion but cell parameters (par) were not provided.")
+        raise ValueError(
+            "Requested crystal conversion but cell parameters (par) were not provided.")
 
     # cases with crystal conversion
     prefactor = 1.0
@@ -165,8 +172,8 @@ def _determine_ftype(filename):
 
     # cases without extensions
     if not ext:
-        base = base.split('/')[-1]
-        if base in ['poscar', 'contcar']:
+        filename = base.split('/')[-1]
+        if filename in ['poscar', 'contcar']:
             return 'vasp'
         elif os.path.exists(base + '.in') and os.path.exists(base + '.out'):
             # filename is a prefix for 'prefix.in' and 'prefix.out'
@@ -187,7 +194,8 @@ def _determine_ftype(filename):
             return ftype
 
     # raise ValueError when determining ftype fails
-    raise ValueError('Unrecognizable extension or unable to determine the type of file for: {}'.format(filename))
+    raise ValueError(
+        'Unrecognizable extension or unable to determine the type of file for: {}'.format(filename))
 
 
 def savetxt_with_maxcol(f, X, columns=6, fmt='%13.6e'):
